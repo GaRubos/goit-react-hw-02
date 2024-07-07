c2bfa48d34fc05b5d599a96c292fb8d30d88ee83
Ruslan Garmash <garmash.rb@gmal.com>
2024-07-07 18:20:46 +0300

    step 2 done

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -13,7 +13,15 @@ import Options from "../Options/Options";
 // };
 
 const App = () => {
-  const [feedbacks, setFeedbacks] = useState({ good: 0, netural: 0, bad: 0 });
+  const [feedbacks, setFeedbacks] = useState({ good: 0, neutral: 0, bad: 0 });
+
+  const updateFeedback = (feedbackType) => {
+    // Тут використовуй сеттер, щоб оновити стан
+    setFeedbacks((prevFeedback) => ({
+      ...prevFeedback,
+      [feedbackType]: prevFeedback[feedbackType] + 1,
+    }));
+  };
 
   return (
     <>
@@ -23,7 +31,7 @@ const App = () => {
           "Please leave your feedback about our service by selecting one of the options below"
         }
       />
-      <Options />
+      <Options updateFeedback={updateFeedback} />
       <Feedback {...feedbacks} />
     </>
   );
